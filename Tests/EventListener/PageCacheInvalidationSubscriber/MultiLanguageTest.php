f57e452ab4b5e17268f3a74c99a05505da316da9
Tommy Juhnke <dev@van-tomas.de>
2016-10-19 09:43:27 +0200

    Add test for multilanguage page cache invalidation

---
+++ Tests/EventListener/PageCacheInvalidationSubscriber/MultiLanguageTest.php
@@ -0,0 +1,213 @@
+<?php
+
+/*
+ * This file is part of the `DreadLabs/KunstmaanDistributedBundle` project.
+ *
+ * (c) https://github.com/DreadLabs/KunstmaanDistributedBundle/graphs/contributors
+ *
+ * For the full copyright and license information, please view the LICENSE.md
+ * file that was distributed with this source code.
+ */
+
+namespace DreadLabs\KunstmaanDistributedBundle\Tests\EventListener\PageCacheInvalidationSubscriber;
+
+use DreadLabs\KunstmaanDistributedBundle\EventListener\PageCacheInvalidationSubscriber;
+use FOS\HttpCacheBundle\CacheManager;
+use Kunstmaan\AdminBundle\Helper\DomainConfigurationInterface;
+use Kunstmaan\NodeBundle\Entity\NodeTranslation;
+use Kunstmaan\NodeBundle\Event\Events;
+use Kunstmaan\NodeBundle\Event\NodeEvent;
+use Symfony\Component\Routing\Generator\UrlGeneratorInterface;
+
+class MultiLanguageTest extends \PHPUnit_Framework_TestCase
+{
+    /**
+     * @var CacheManager|\PHPUnit_Framework_MockObject_MockObject
+     */
+    protected $cacheManager;
+
+    /**
+     * @var DomainConfigurationInterface|\PHPUnit_Framework_MockObject_MockObject
+     */
+    protected $domainConfiguration;
+
+    /**
+     * @var UrlGeneratorInterface|\PHPUnit_Framework_MockObject_MockObject
+     */
+    protected $urlGenerator;
+
+    /**
+     * @var NodeEvent|\PHPUnit_Framework_MockObject_MockObject
+     */
+    protected $nodeEvent;
+
+    /**
+     * @var NodeTranslation|\PHPUnit_Framework_MockObject_MockObject
+     */
+    protected $nodeTranslation;
+
+    public function setUp()
+    {
+        $this->cacheManager = $this->getMockBuilder(CacheManager::class)->disableOriginalConstructor()->getMock();
+        $this->domainConfiguration = $this->getMockBuilder(DomainConfigurationInterface::class)->disableOriginalConstructor()->getMock();
+        $this->urlGenerator = $this->getMockBuilder(UrlGeneratorInterface::class)->disableOriginalConstructor()->getMock();
+
+        $this->nodeEvent = $this->getMockBuilder(NodeEvent::class)->disableOriginalConstructor()->getMock();
+        $this->nodeTranslation = $this->getMockBuilder(NodeTranslation::class)->disableOriginalConstructor()->getMock();
+    }
+
+    /**
+     * @test
+     */
+    public function eventsSubscription()
+    {
+        $events = PageCacheInvalidationSubscriber::getSubscribedEvents();
+
+        $this->assertCount(3, $events);
+
+        $this->assertArrayHasKey(Events::POST_UNPUBLISH, $events);
+        $this->assertArrayHasKey(Events::POST_DELETE, $events);
+        $this->assertArrayHasKey(Events::POST_PERSIST, $events);
+    }
+
+    /**
+     * @test
+     */
+    public function pathInvalidatonOnPageUnpublishing()
+    {
+        $this->cacheManager
+            ->expects($this->once())
+            ->method('invalidatePath')
+            ->with($this->equalTo('de/path/to/page'))
+            ->willReturn($this->cacheManager);
+        $this->domainConfiguration
+            ->expects($this->once())
+            ->method('isMultiLanguage')
+            ->willReturn(true);
+        $this->urlGenerator
+            ->expects($this->once())
+            ->method('generate')
+            ->with($this->equalTo('_slug'), $this->equalTo(['url' => 'path/to/page', '_locale' => 'de']))
+            ->willReturn('de/path/to/page');
+
+        $this->nodeEvent
+            ->expects($this->once())
+            ->method('getNodeTranslation')
+            ->willReturn($this->nodeTranslation);
+
+        $this->nodeTranslation
+            ->expects($this->once())
+            ->method('getUrl')
+            ->willReturn('path/to/page');
+
+        $this->nodeTranslation
+            ->expects($this->once())
+            ->method('getLang')
+            ->willReturn('de');
+
+        $this->cacheManager
+            ->expects($this->once())
+            ->method('flush');
+
+        $subscriber = new PageCacheInvalidationSubscriber(
+            $this->cacheManager,
+            $this->domainConfiguration,
+            $this->urlGenerator
+        );
+        $subscriber->onUnpublishPage($this->nodeEvent);
+    }
+
+    /**
+     * @test
+     */
+    public function pathInvalidationOnPageDeletion()
+    {
+        $this->cacheManager
+            ->expects($this->once())
+            ->method('invalidatePath')
+            ->with($this->equalTo('de/path/to/another/page'))
+            ->willReturn($this->cacheManager);
+        $this->domainConfiguration
+            ->expects($this->once())
+            ->method('isMultiLanguage')
+            ->willReturn(true);
+        $this->urlGenerator
+            ->expects($this->once())
+            ->method('generate')
+            ->with($this->equalTo('_slug'), $this->equalTo(['url' => 'path/to/another/page', '_locale' => 'de']))
+            ->willReturn('de/path/to/another/page');
+
+        $this->nodeEvent
+            ->expects($this->once())
+            ->method('getNodeTranslation')
+            ->willReturn($this->nodeTranslation);
+
+        $this->nodeTranslation
+            ->expects($this->once())
+            ->method('getUrl')
+            ->willReturn('path/to/another/page');
+
+        $this->nodeTranslation
+            ->expects($this->once())
+            ->method('getLang')
+            ->willReturn('de');
+
+        $this->cacheManager
+            ->expects($this->once())
+            ->method('flush');
+
+        $subscriber = new PageCacheInvalidationSubscriber(
+            $this->cacheManager,
+            $this->domainConfiguration,
+            $this->urlGenerator
+        );
+        $subscriber->onDeletePage($this->nodeEvent);
+    }
+
+    /**
+     * @test
+     */
+    public function pathInvalidationOnPageUpdate()
+    {
+        $this->cacheManager
+            ->expects($this->once())
+            ->method('invalidatePath')
+            ->with($this->equalTo('de/path/to/page/three'))
+            ->willReturn($this->cacheManager);
+        $this->domainConfiguration
+            ->expects($this->once())
+            ->method('isMultiLanguage')
+            ->willReturn(true);
+        $this->urlGenerator
+            ->expects($this->once())
+            ->method('generate')
+            ->with($this->equalTo('_slug'), $this->equalTo(['url' => 'path/to/page/three', '_locale' => 'de']))
+            ->willReturn('de/path/to/page/three');
+
+        $this->nodeEvent
+            ->expects($this->once())
+            ->method('getNodeTranslation')
+            ->willReturn($this->nodeTranslation);
+
+        $this->nodeTranslation
+            ->expects($this->once())
+            ->method('getUrl')
+            ->willReturn('path/to/page/three');
+
+        $this->nodeTranslation
+            ->expects($this->once())
+            ->method('getLang')
+            ->willReturn('de');
+
+        $this->cacheManager
+            ->expects($this->once())
+            ->method('flush');
+
+        $subscriber = new PageCacheInvalidationSubscriber(
+            $this->cacheManager,
+            $this->domainConfiguration,
+            $this->urlGenerator
+        );
+        $subscriber->onUpdatePage($this->nodeEvent);
+    }
+}
