0ffbbe67f07a1f94222eb7148ec27329679fe767
Tommy Juhnke <dev@van-tomas.de>
2016-10-19 09:34:32 +0200

    Run php-cs-fixer

--- tests/HttpCache/HttpCacheTest.php
+++ tests/HttpCache/HttpCacheTest.php
@@ -1,5 +1,14 @@
 <?php
 
+/*
+ * This file is part of the `DreadLabs/KunstmaanDistributedBundle` project.
+ *
+ * (c) https://github.com/DreadLabs/KunstmaanDistributedBundle/graphs/contributors
+ *
+ * For the full copyright and license information, please view the LICENSE.md
+ * file that was distributed with this source code.
+ */
+
 namespace DreadLabs\KunstmaanDistributedBundle\Tests\HttpCache;
 
 use DreadLabs\KunstmaanDistributedBundle\HttpCache\HttpCache;
